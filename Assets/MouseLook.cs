6ebb7fcdee02525b226dd72d1aae826a8c94d379
JustJohn:) <bushytreeman@gmail.com>
2021-02-08 16:55:08 -0800

    first commit

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour  
+{
+    //obviously how fast you look around, change to preference :)
+    public float mouseSensitivity = 10000f;
+    //this is so you can link the camera to the body
+    public Transform playerBody;
+    //start of moving y axis
+    float xRotation = 0f;
+
+    
+    void Start()
+    {
+        //This code will hide and lock cursor so if you go past the screen with your mouse, it wouldn't end up on another monitor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    
+    void Update()
+    {
+        //"Mouse X" and "Mouse Y" are Unity's base mouse input system
+        //this is basically so you can access moving the camera with the mouse
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        //to get the y axis to work properly, the minus is so your controls are not inverted
+        xRotation -= mouseY;
+        //so your pov doesnt go upside down when looking all the way up, or down
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        //this helps with the rotation of the x axis
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        //To get the x axis to work properly
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //This will tell the game where youre moving to, whether using keyboard or controller (basically driving the character)
+    public CharacterController controller;
+    //the speed of character
+    public float speed = 12f;
+    //Base speed of gravity for falling
+    public float gravity = -9.81f;
+    //Jump Height
+    public float jumpHeight = 3f;
+    //So the velocity resets on character so they dont drop very quick
+    public Transform groundCheck;
+    //Size of the indicator to let you know you landed to reset velocity
+    public float groundDistance = 0.4f;
+    //will check to see if youre on ground instead of other objects
+    public LayerMask groundMask;
+    //for the gravity of the character
+    Vector3 velocity;
+    //will know if youre grounded
+    bool isGrounded;
+    
+    void Update()
+    {
+        //will let code know if landed
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        //this is player input Prep
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        //Taking the input and turning it into directions for movement
+        Vector3 move = transform.right * x + transform.forward * z;
+        //will actually allow movement
+        controller.Move(move * speed * Time.deltaTime);
+        //Jump Controlls
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //to get the gravity to actually work
+        velocity.y += gravity * Time.deltaTime;
+        //to get a realistic gravity
+        controller.Move(velocity * Time.deltaTime);
+
+
+    }
+}
